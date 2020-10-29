578ba937ef9e6bd0e4562e5e36d61cda4447e050
MatheusNani <matheus.nani@hotmail.com>
2020-10-29 08:54:06 -0300

    login and home base

--- client/src/app/components/nav/nav.component.ts
+++ client/src/app/components/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { AccountService } from './../services/account.service';
+import { AccountService } from './../../services/account.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,9 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
 
   model: any = {};
-  loggedIn: boolean;
-
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
   }
@@ -19,10 +17,13 @@ export class NavComponent implements OnInit {
   login() {
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn = true;
     }, error => {
       console.log(error);
     });
+  }
+
+  logout() {
+    this.accountService.logout();
   }
 
 }
