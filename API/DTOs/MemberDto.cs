f4b0e25f963128a189ead3a45d9d94b0e1261d67
Matheus Nani <matheus.nani@hotmail.com>
2020-11-16 23:11:29 -0300

    adding user detail

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -32,6 +32,8 @@ namespace API.DTOs
 
         public string Country { get; set; }
 
+        public string City { get; set; }
+
         public ICollection<PhotoDto> Photos { get; set; }
     }
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -34,11 +34,8 @@ namespace API.Entities
 
         public string Country { get; set; }
 
-        public ICollection<Photo> Photos { get; set; }
+        public string City { get; set; }
 
-        //public int GetAge()
-        //{
-        //    return DateOfBirth.CalculateAge();
-        //}
+        public ICollection<Photo> Photos { get; set; }
     }
 }
